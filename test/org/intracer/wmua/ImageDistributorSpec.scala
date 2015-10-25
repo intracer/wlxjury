f41913252d3c4cfcf5bbe8f2ae2969f17b11518a
ilya <intracer@gmail.com>
2015-10-25 11:48:12 +0200

    create first round 2 jurors to image

--- test/org/intracer/wmua/ImageDistributorSpec.scala
+++ test/org/intracer/wmua/ImageDistributorSpec.scala
@@ -20,6 +20,8 @@ class ImageDistributorSpec extends Specification {
     running(FakeApplication(additionalConfiguration = inMemoryDatabase()))(block)
   }
 
+  val (contest1, contest2) = (10, 20)
+
   def contestImage(id: Long, contest: Long) =
     Image(id, contest, s"File:Image$id.jpg", s"url$id", s"pageUrl$id", 640, 480, Some(s"12-345-$id"))
 
@@ -27,11 +29,11 @@ class ImageDistributorSpec extends Specification {
     User("fullname", "email", None, Set(role), Some("password hash"), contest, Some("en"))
 
   def createJurors(
-                   contest: Long,
-                   jurorsNum: Int,
-                   preJurors: Boolean = true,
-                   orgCom: Boolean = true,
-                   otherContest: Option[Long] = Some(20)) = {
+                    contest: Long,
+                    jurorsNum: Int,
+                    preJurors: Boolean = true,
+                    orgCom: Boolean = true,
+                    otherContest: Option[Long] = Some(20)) = {
 
     val jurors = (1 to jurorsNum).map(i => contestUser(contest, "jury"))
     val dbJurors = jurors.map(userDao.create).map(u => u.copy(roles = u.roles + s"USER_ID_${u.id.get}"))
@@ -48,18 +50,23 @@ class ImageDistributorSpec extends Specification {
     dbJurors
   }
 
+  def createImages(number: Int, contest1: Long, contest2: Long) = {
+    val images1 = (101 to 100 + number).map(id => contestImage(id, contest1))
+    val images2 = (100 + number + 1 to 100 + number * 2).map(id => contestImage(id, contest2))
+
+    imageDao.batchInsert(images1 ++ images2)
+
+    images1
+  }
+
   "ImageDistributorTest" should {
     "create first round 1 juror to image" in {
       inMemDbApp {
+        val distribution = 1
 
-        val (contest1, contest2) = (10, 20)
+        val images = createImages(9, contest1, contest2)
 
-        val images1 = (11 to 19).map(id => contestImage(id, contest1))
-        val images2 = (21 to 29).map(id => contestImage(id, contest2))
-
-        imageDao.batchInsert(images1 ++ images2)
-
-        val round = Round(None, 1, Some("Round 1"), 10, Set("jury"), 1, Round.ratesById(10), active = true)
+        val round = Round(None, 1, Some("Round 1"), contest1, Set("jury"), distribution, Round.ratesById(10), active = true)
         val dbRound = roundDao.create(round)
 
         val dbJurors = createJurors(contest1, 3)
@@ -73,13 +80,41 @@ class ImageDistributorSpec extends Specification {
 
         selection.map(_.round).toSet === Set(dbRound.id.get)
         selection.map(_.rate).toSet === Set(0)
-        selection.map(_.pageId) === images1.map(_.pageId)
+        selection.map(_.pageId) === images.map(_.pageId)
         selection.map(_.juryId) === juryIds ++ juryIds ++ juryIds
       }
     }
 
-    "distributeImages" in {
-      ok
+    "create first round 2 jurors to image" in {
+      inMemDbApp {
+        val distribution = 2
+        val numImages = 9
+        val numJurors = 3
+        val images = createImages(numImages, contest1, contest2)
+
+        val round = Round(None, 1, Some("Round 1"), contest1, Set("jury"), distribution, Round.ratesById(10), active = true)
+        val dbRound = roundDao.create(round)
+
+        val dbJurors = createJurors(contest1, numJurors)
+        val juryIds = dbJurors.map(_.id.get)
+
+        ImageDistributor.distributeImages(contest1, dbRound)
+
+        val selection = selectionDao.findAll()
+
+        selection.size === numImages * distribution
+
+        selection.map(_.round).toSet === Set(dbRound.id.get)
+        selection.map(_.rate).toSet === Set(0)
+
+        val jurorsPerImage = selection.groupBy(_.pageId).values.map(_.size)
+        jurorsPerImage.size === numImages
+        jurorsPerImage.toSet === Set(2)
+
+        val imagesPerJuror = selection.groupBy(_.juryId).values.map(_.size)
+        imagesPerJuror.size === numJurors
+        imagesPerJuror.toSet === Set(distribution * numImages / numJurors)
+      }
     }
   }
 }
