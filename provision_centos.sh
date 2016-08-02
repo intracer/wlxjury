cc75a620496d9dadb562aac64da0dc55cf65edfd
intracer <intracer@gmail.com>
2016-08-02 11:13:04 +0300

    install rpm-build

--- provision_centos.sh
+++ provision_centos.sh
@@ -5,19 +5,19 @@ DB_USER_NAME=wlx_jury_user
 DB_USER_PASSW=wlx_jury
 
 rpm --quiet -q java-1.8.0-openjdk
-if [ $? != 0 ]  ; then
+if [ $? != 0 ] ; then
     yum -y install java-1.8.0-openjdk*
 fi
 
 rpm --quiet -q sbt
-if [ $? != 0 ]  ; then
+if [ $? != 0 ] ; then
     curl https://bintray.com/sbt/rpm/rpm > bintray-sbt-rpm.repo
     mv bintray-sbt-rpm.repo /etc/yum.repos.d/
     yum -y install sbt
 fi
 
 rpm --quiet -q mariadb-server
-if [ $? != 0 ]  ; then
+if [ $? != 0 ] ; then
     yum -y install mariadb-server
     systemctl start mariadb
     systemctl enable mariadb
@@ -27,11 +27,16 @@ if ! mysql -u root -e "use $DB_NAME"; then
     mysql -u root -e "create database $DB_NAME; GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER_NAME@localhost IDENTIFIED BY '$DB_USER_PASSW'"
 fi
 
+rpm --quiet -q rpm-build
+if [ $? != 0 ] ; then
+    yum -y install rpm-build
+fi
+
 cd /vagrant
 sbt -v clean packageRpmSystemd
 
 rpm --quiet -q wlxjury
-if [ $? = 0 ]  ; then
+if [ $? = 0 ] ; then
     rpm -e wlxjury
 fi
 
