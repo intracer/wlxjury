bb7602917c7e3791ec82ae4f6d5dab8dbb09360c
intracer <intracer@gmail.com>
2016-10-11 23:51:36 +0300

    SelectionSpec more tests

--- test/db/scalikejdbc/SelectionSpec.scala
+++ test/db/scalikejdbc/SelectionSpec.scala
@@ -2,6 +2,7 @@ package db.scalikejdbc
 
 import db.SelectionDao
 import org.intracer.wmua.Selection
+import org.joda.time.DateTime
 import org.specs2.mutable.Specification
 
 class SelectionSpec extends Specification with InMemDb {
@@ -9,6 +10,11 @@ class SelectionSpec extends Specification with InMemDb {
   sequential
 
   val selectionDao: SelectionDao = SelectionJdbc
+  val time = DateTime.now
+
+  def sameTime(s: Seq[Selection]) = s.map(_.copy(createdAt = time))
+
+  def findAll() = sameTime(selectionDao.findAll())
 
   "fresh database" should {
     "be empty" in {
@@ -36,5 +42,47 @@ class SelectionSpec extends Specification with InMemDb {
         selections === Seq(created)
       }
     }
+
+    "batch insert selections" in {
+      inMemDbApp {
+        val s = sameTime(Seq(
+          Selection(-1, 1, 0, 10, 20),
+          Selection(-1, 2, 1, 11, 21),
+          Selection(-1, 3, -1, 12, 22)
+        ))
+
+        selectionDao.batchInsert(s)
+
+        val selections = sameTime(selectionDao.findAll())
+        selections.map(_.id) === Seq(1,2,3)
+        selections.map(_.copy(id= -1)) === s
+      }
+    }
+
+    "rate selections" in {
+
+      inMemDbApp {
+        val s = sameTime(Seq(
+          Selection(1, 1, 0, 10, 20),
+          Selection(2, 1, 0, 11, 20),
+          Selection(3, 2, 0, 10, 20),
+          Selection(4, 2, 0, 11, 20),
+          Selection(5, 1, 0, 10, 21)
+        ))
+
+        selectionDao.batchInsert(s)
+
+        selectionDao.rate(1, 10, 20, 1)
+
+        findAll() === s.head.copy(rate = 1) +: s.tail
+
+        selectionDao.rate(1, 10, 20, -1)
+        findAll() === s.head.copy(rate = -1) +: s.tail
+
+        selectionDao.rate(1, 10, 20, 0)
+        findAll() === s
+      }
+    }
+
   }
 }
