97d4f22d60f57fb14a3e6dfd176cdf15db2463b0
intracer <intracer@gmail.com>
2016-11-10 12:43:09 +0200

    error message with status code and in error view

--- app/ErrorHandler.scala
+++ app/ErrorHandler.scala
@@ -1,4 +1,4 @@
-import play.api.Logger
+import play.api.{Logger, mvc}
 import play.api.http.HttpErrorHandler
 import play.api.mvc._
 import play.api.mvc.Results._
@@ -9,10 +9,10 @@ import play.api.i18n.Messages.Implicits._
 class ErrorHandler extends HttpErrorHandler {
 
   def onClientError(request: RequestHeader, statusCode: Int, message: String) = {
-    val fullMessage = "A client error occurred: " + message
+    val fullMessage = "An error occurred: " + statusCode + (if (message.nonEmpty) ", " + message else "")
     Logger.logger.error(fullMessage)
     Future.successful(
-      Status(statusCode)(fullMessage)
+      Status(statusCode)(views.html.error(fullMessage))
     )
   }
 
