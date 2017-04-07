f5896ed12ee7b02315f84ab8be4e2fe2f79181d3
ilya <intracer@gmail.com>
2017-04-07 14:11:49 +0300

    add javadoc

--- app/modules/LogSdtErrLoader.scala
+++ app/modules/LogSdtErrLoader.scala
@@ -4,6 +4,9 @@ import play.api.ApplicationLoader
 import play.api.inject.guice.{GuiceApplicationBuilder, GuiceApplicationLoader}
 import uk.org.lidalia.sysoutslf4j.context.SysOutOverSLF4J
 
+/**
+  * For http://stackoverflow.com/questions/38736689/how-to-log-play-framework-startup-errors
+  */
 class LogSdtErrLoader extends GuiceApplicationLoader {
 
   override final def builder(context: ApplicationLoader.Context): GuiceApplicationBuilder = {
