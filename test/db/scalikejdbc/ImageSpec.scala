db8c629f38bb3eeb8e43405f5a16c29a3f5fa248
intracer <intracer@gmail.com>
2017-08-23 09:16:55 +0300

    contests can share images implement test

--- test/db/scalikejdbc/ImageSpec.scala
+++ test/db/scalikejdbc/ImageSpec.scala
@@ -66,20 +66,23 @@ class ImageSpec extends Specification with InMemDb {
       }
     }
 
-    "contest can share images" in {
+    "contests can share images" in {
       inMemDbApp {
         val (contest1, contest2) = (10, 20)
         createContests(contest1, contest2)
 
         val images1 = (11 to 19).map(id => image(id))
         val images2 = (21 to 29).map(id => image(id))
+        val commonImages = (31 to 39).map(id => image(id))
 
-        imageDao.batchInsert(images1 ++ images2)
+        imageDao.batchInsert(images1 ++ images2 ++ commonImages)
         addToContest(contest1, images1)
         addToContest(contest2, images2)
+        addToContest(contest1, commonImages)
+        addToContest(contest2, commonImages)
 
-        imageDao.findByContestId(10) === images1
-        imageDao.findByContestId(20) === images2
+        imageDao.findByContestId(10) === images1 ++ commonImages
+        imageDao.findByContestId(20) === images2 ++ commonImages
       }
     }
 
