f8150c83b3265e6dc3c4374ea9207ef6646850a1
ilya <intracer@gmail.com>
2018-06-09 14:32:55 +0300

    fix SelectionSpec

--- test/db/scalikejdbc/SelectionSpec.scala
+++ test/db/scalikejdbc/SelectionSpec.scala
@@ -11,6 +11,7 @@ class SelectionSpec extends Specification with InMemDb {
   val time = now
 
   def sameTime(s: Seq[Selection]) = s.map(_.copy(createdAt = Some(time)))
+  def noIds(s: Seq[Selection]) = s.map(_.copy(id = None))
 
   def findAll() = sameTime(selectionDao.findAll())
 
@@ -44,16 +45,16 @@ class SelectionSpec extends Specification with InMemDb {
     "batch insert selections" in {
       inMemDbApp {
         val s = sameTime(Seq(
-          Selection(-1, 1, 0, 10, Some(20)),
-          Selection(-1, 2, 1, 11, Some(21)),
-          Selection(-1, 3, -1, 12, Some(22))
+          Selection(20, 1, 0, 10),
+          Selection(21, 2, 1, 11),
+          Selection(22, 3, -1, 12)
         ))
 
         selectionDao.batchInsert(s)
 
         val selections = sameTime(selectionDao.findAll())
-        selections.map(_.id) === Seq(1,2,3)
-        selections.map(_.copy()) === s
+        selections.flatMap(_.id) === Seq(1,2,3)
+        noIds(selections) === s
       }
     }
 
@@ -61,24 +62,24 @@ class SelectionSpec extends Specification with InMemDb {
 
       inMemDbApp {
         val s = sameTime(Seq(
-          Selection(1, 1, 0, 10, Some(20)),
-          Selection(2, 1, 0, 11, Some(20)),
-          Selection(3, 2, 0, 10, Some(20)),
-          Selection(4, 2, 0, 11, Some(20)),
-          Selection(5, 1, 0, 10, Some(21))
+          Selection(pageId = 1, roundId = 20, juryId = 10),
+          Selection(pageId = 1, roundId = 20, juryId = 11),
+          Selection(pageId = 2, roundId = 20, juryId = 10),
+          Selection(pageId = 2, roundId = 20, juryId = 11),
+          Selection(pageId = 1, roundId = 21, juryId = 10)
         ))
 
         selectionDao.batchInsert(s)
 
         selectionDao.rate(1, 10, 20, 1)
 
-        findAll() === s.head.copy(rate = 1) +: s.tail
+        noIds(findAll()) === s.head.copy(rate = 1) +: s.tail
 
         selectionDao.rate(1, 10, 20, -1)
-        findAll() === s.head.copy(rate = -1) +: s.tail
+        noIds(findAll()) === s.head.copy(rate = -1) +: s.tail
 
         selectionDao.rate(1, 10, 20, 0)
-        findAll() === s
+        noIds(findAll()) === s
       }
     }
 
