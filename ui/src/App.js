b6e6bd76cda96bfddd38e78cddaf8a480ac2b280
ilya <intracer@gmail.com>
2018-06-09 19:12:43 +0300

    added select-image

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Gallery from 'react-photo-gallery';
-import Lightbox from 'react-images';
+import SelectedImage from './SelectedImage';
 
 const photos = [
     { src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599', width: 4, height: 3 },
@@ -14,49 +14,27 @@ const photos = [
     { src: 'https://source.unsplash.com/I1ASdgphUH4/800x599', width: 4, height: 3 }
 ];
 
-
 class App extends React.Component {
-    constructor() {
-        super();
-        this.state = { currentImage: 0 };
-        this.closeLightbox = this.closeLightbox.bind(this);
-        this.openLightbox = this.openLightbox.bind(this);
-        this.gotoNext = this.gotoNext.bind(this);
-        this.gotoPrevious = this.gotoPrevious.bind(this);
+    constructor(props) {
+        super(props);
+        this.state = { photos: photos, selectAll: false };
+        this.selectPhoto = this.selectPhoto.bind(this);
+        this.toggleSelect = this.toggleSelect.bind(this);
     }
-    openLightbox(event, obj) {
-        this.setState({
-            currentImage: obj.index,
-            lightboxIsOpen: true,
-        });
+    selectPhoto(event, obj) {
+        let photos = this.state.photos;
+        photos[obj.index].selected = !photos[obj.index].selected;
+        this.setState({ photos: photos });
     }
-    closeLightbox() {
-        this.setState({
-            currentImage: 0,
-            lightboxIsOpen: false,
-        });
-    }
-    gotoPrevious() {
-        this.setState({
-            currentImage: this.state.currentImage - 1,
-        });
-    }
-    gotoNext() {
-        this.setState({
-            currentImage: this.state.currentImage + 1,
-        });
+    toggleSelect() {
+        let photos = this.state.photos.map((photo, index) => { return { ...photo, selected: !this.state.selectAll } });
+        this.setState({ photos: photos, selectAll: !this.state.selectAll });
     }
     render() {
         return (
             <div>
-                <Gallery photos={photos} onClick={this.openLightbox} />
-                <Lightbox images={photos}
-                          onClose={this.closeLightbox}
-                          onClickPrev={this.gotoPrevious}
-                          onClickNext={this.gotoNext}
-                          currentImage={this.state.currentImage}
-                          isOpen={this.state.lightboxIsOpen}
-                />
+                <p><button className="toggle-select" onClick={this.toggleSelect}>toggle select all</button></p>
+                <Gallery photos={this.state.photos} onClick={this.selectPhoto} ImageComponent={SelectedImage} />
             </div>
         )
     }
