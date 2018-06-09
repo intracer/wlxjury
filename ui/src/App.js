a1b5eb48013a05e796b8fe998bd7172ad215cb8e
ilya <intracer@gmail.com>
2018-06-09 19:10:10 +0300

    added lightbox

--- ui/src/App.js
+++ ui/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { render } from 'react-dom';
 import Gallery from 'react-photo-gallery';
+import Lightbox from 'react-images';
 
 const photos = [
     { src: 'https://source.unsplash.com/2ShvY8Lf6l0/800x599', width: 4, height: 3 },
@@ -14,10 +14,50 @@ const photos = [
     { src: 'https://source.unsplash.com/I1ASdgphUH4/800x599', width: 4, height: 3 }
 ];
 
+
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = { currentImage: 0 };
+        this.closeLightbox = this.closeLightbox.bind(this);
+        this.openLightbox = this.openLightbox.bind(this);
+        this.gotoNext = this.gotoNext.bind(this);
+        this.gotoPrevious = this.gotoPrevious.bind(this);
+    }
+    openLightbox(event, obj) {
+        this.setState({
+            currentImage: obj.index,
+            lightboxIsOpen: true,
+        });
+    }
+    closeLightbox() {
+        this.setState({
+            currentImage: 0,
+            lightboxIsOpen: false,
+        });
+    }
+    gotoPrevious() {
+        this.setState({
+            currentImage: this.state.currentImage - 1,
+        });
+    }
+    gotoNext() {
+        this.setState({
+            currentImage: this.state.currentImage + 1,
+        });
+    }
     render() {
         return (
-            <Gallery photos={photos} />
+            <div>
+                <Gallery photos={photos} onClick={this.openLightbox} />
+                <Lightbox images={photos}
+                          onClose={this.closeLightbox}
+                          onClickPrev={this.gotoPrevious}
+                          onClickNext={this.gotoNext}
+                          currentImage={this.state.currentImage}
+                          isOpen={this.state.lightboxIsOpen}
+                />
+            </div>
         )
     }
 }
