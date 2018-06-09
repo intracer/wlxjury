bcc8c27c4470ceb6db105e7e10cca4cfa104be73
ilya <intracer@gmail.com>
2018-06-09 17:20:28 +0300

    FrontendCommands

---
+++ project/FrontendCommands.scala
@@ -0,0 +1,9 @@
+/**
+  * Frontend build commands.
+  */
+object FrontendCommands {
+  val dependencyInstall: String = "npm install"
+  val test: String = "npm run test"
+  val serve: String = "npm run start"
+  val build: String = "npm run build"
+}
