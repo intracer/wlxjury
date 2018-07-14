3e0339ce72fca366cb4d7dfaea19af960c239cd1
ilya <intracer@gmail.com>
2018-07-14 16:24:03 +0300

    more large view tests

--- test/controllers/LargeImageSpec.scala
+++ test/controllers/LargeImageSpec.scala
@@ -69,17 +69,69 @@ class LargeImageSpec extends PlaySpecification with InMemDb {
         .withHeaders("Accept" -> "application/json")
         .withCSRFToken
 
-      val result = LargeView.large(user.id.get, images.head.pageId, roundId = round.id.get, rate = None, module = "byrate").apply(request)
+      val result = LargeView.large(user.id.get, images.head.pageId, roundId = round.id.get, rate = None, module = "gallery").apply(request)
 
       status(result) mustEqual OK
       contentAsJson(result) mustEqual
-        Json.parse("""[{"image":{"pageId":0,"title":"File:Image0.jpg","width":640,"height":480,"monumentId":"12-345-0"},
-          |"selection":[{"pageId":0,"juryId":1,"roundId":1,"rate":0,"id":1}],
-          |"countFromDb":0}]""".stripMargin)
+        Json.parse(
+          """[{"image":{"pageId":0,"title":"File:Image0.jpg","width":640,"height":480,"monumentId":"12-345-0"},
+            |"selection":[{"pageId":0,"juryId":1,"roundId":1,"rate":0,"id":1}],
+            |"countFromDb":0}]""".stripMargin)
     }
   }
 
-  "rate 1 image" in {
+  "get 2 unrated images" in {
+    inMemDbApp { app =>
+      implicit val materializer = app.materializer
+
+      setUp(rates = Round.ratesById(5))
+
+      val images = createImages(2)
+      val selection = createSelection(images)
+
+      val request = FakeRequest(GET, s"/large/round/${round.id.get}/user/${user.id.get}/pageid/${images.head.pageId}?rate=0")
+        .withSession(Security.username -> email)
+        .withHeaders("Accept" -> "application/json")
+        .withCSRFToken
+
+      val result = LargeView.large(user.id.get, images.head.pageId, roundId = round.id.get, rate = None, module = "gallery").apply(request)
+
+      status(result) mustEqual OK
+      contentAsJson(result) mustEqual
+        Json.parse(
+          """[{"image":{"pageId":0,"title":"File:Image0.jpg","width":640,"height":480,"monumentId":"12-345-0"},
+            |"selection":[{"pageId":0,"juryId":1,"roundId":1,"rate":0,"id":1}],"countFromDb":0},
+            |{"image":{"pageId":1,"title":"File:Image1.jpg","width":640,"height":480,"monumentId":"12-345-1"},
+            |"selection":[{"pageId":1,"juryId":1,"roundId":1,"rate":0,"id":2}],"countFromDb":0}]""".stripMargin)
+    }
+  }
+
+  "get last unrated image from 2" in {
+    inMemDbApp { app =>
+      implicit val materializer = app.materializer
+
+      setUp(rates = Round.ratesById(5))
+
+      val images = createImages(2)
+      createSelection(images.init, 5)
+      createSelection(images.tail, 0)
+
+      val request = FakeRequest(GET, s"/large/round/${round.id.get}/user/${user.id.get}/pageid/${images.last.pageId}?rate=0")
+        .withSession(Security.username -> email)
+        .withHeaders("Accept" -> "application/json")
+        .withCSRFToken
+
+      val result = LargeView.large(user.id.get, images.last.pageId, roundId = round.id.get, rate = Some(0), module = "gallery").apply(request)
+
+      status(result) mustEqual OK
+      contentAsJson(result) mustEqual
+        Json.parse(
+          """[{"image":{"pageId":1,"title":"File:Image1.jpg","width":640,"height":480,"monumentId":"12-345-1"},
+            |"selection":[{"pageId":1,"juryId":1,"roundId":1,"rate":0,"id":2}],"countFromDb":0}]""".stripMargin)
+    }
+  }
+
+  "rate 1 unrated image" in {
     inMemDbApp { app =>
       implicit val materializer = app.materializer
 
@@ -88,15 +140,15 @@ class LargeImageSpec extends PlaySpecification with InMemDb {
       val images = createImages(1)
       val selection = createSelection(images)
 
-      val request = FakeRequest(GET, s"/large/round/${round.id.get}/user/${user.id.get}/pageid/${images.head.pageId}/select/2?module=byrate")
+      val request = FakeRequest(GET, s"/large/round/${round.id.get}/user/${user.id.get}/pageid/${images.head.pageId}/select/5?rate=0")
         .withSession(Security.username -> email)
         .withHeaders("Accept" -> "application/json")
         .withCSRFToken
 
-      val result = LargeView.rateByPageId(round.id.get, images.head.pageId, select = 5, module = "byrate").apply(request)
+      val result = LargeView.rateByPageId(round.id.get, images.head.pageId, select = 5, module = "gallery", rate = Some(0)).apply(request)
 
       status(result) mustEqual SEE_OTHER
-      redirectLocation(result) must beSome.which(_ == s"/byrate/round/${round.id.get}/user/${user.id.get}/page/1")
+      redirectLocation(result) must beSome.which(_ == s"/gallery/round/${round.id.get}/user/${user.id.get}/page/1?rate=0")
     }
   }
 
