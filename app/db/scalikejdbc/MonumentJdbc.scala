9d0595198e22b69debd457560d94e8ce090cc299
ilya <intracer@gmail.com>
2024-11-19 19:50:11 +0200

    upsert monuments

--- app/db/scalikejdbc/MonumentJdbc.scala
+++ app/db/scalikejdbc/MonumentJdbc.scala
@@ -7,7 +7,7 @@ object MonumentJdbc extends SQLSyntaxSupport[Monument]{
 
   override val tableName = "monument"
 
-  val c = MonumentJdbc.syntax("c")
+  private val c = MonumentJdbc.syntax("c")
 
   def apply(c: SyntaxProvider[Monument])(rs: WrappedResultSet): Monument = apply(c.resultName)(rs)
 
@@ -32,7 +32,12 @@ object MonumentJdbc extends SQLSyntaxSupport[Monument]{
     contest = rs.longOpt(c.contest)
   )
 
-  def batchInsert(monuments: Seq[Monument]) = {
+  object replace {
+    def into(support: SQLSyntaxSupport[?]): InsertSQLBuilder =
+      InsertSQLBuilder(sqls"replace into ${support.table}")
+  }
+
+  def batchInsert(monuments: Seq[Monument]): Unit = {
     val column = MonumentJdbc.column
     DB localTx { implicit session =>
       val batchParams: Seq[Seq[Any]] = monuments.map(m => Seq(
@@ -52,7 +57,7 @@ object MonumentJdbc extends SQLSyntaxSupport[Monument]{
         m.id.split("-").headOption.map(_.take(3))
       ))
       withSQL {
-        insert.into(MonumentJdbc).namedValues(
+        replace.into(MonumentJdbc).namedValues(
           column.id -> sqls.?,
           column.name -> sqls.?,
           column.description -> sqls.?,
