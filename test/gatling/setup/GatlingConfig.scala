8208a67b21d07ca1ed3f9cf38a5f6ee37f4dacc1
Illia Korniiko <intracer@gmail.com>
2026-03-26 23:49:20 +0200

    feat: add gatling.quick flag for fast iteration (3 users, 10s duration)

--- test/gatling/setup/GatlingConfig.scala
+++ test/gatling/setup/GatlingConfig.scala
@@ -1,18 +1,18 @@
 package gatling.setup
 
 import com.typesafe.config.ConfigFactory
-
 import java.io.File
 
 object GatlingConfig {
-  private val cfg =
-    ConfigFactory.systemProperties()
-      .withFallback(ConfigFactory.parseFile(new File("test/resources/gatling-perf.conf")))
-      .resolve()
+  private val cfg = ConfigFactory.systemProperties()
+    .withFallback(ConfigFactory.parseFile(new File("test/resources/gatling-perf.conf")))
+    .resolve()
 
-  val users: Int           = cfg.getInt("gatling.users")
-  val rampUpSeconds: Int   = cfg.getInt("gatling.rampUpSeconds")
-  val durationSeconds: Int = cfg.getInt("gatling.durationSeconds")
-  val jurorFraction: Double= cfg.getDouble("gatling.jurorFraction")
-  val maxRate: Int         = cfg.getInt("gatling.maxRate")
+  private val quick = cfg.getBoolean("gatling.quick")
+
+  val users: Int            = if (quick) cfg.getInt("gatling.quickUsers")           else cfg.getInt("gatling.users")
+  val rampUpSeconds: Int    = if (quick) cfg.getInt("gatling.quickRampUpSeconds")   else cfg.getInt("gatling.rampUpSeconds")
+  val durationSeconds: Int  = if (quick) cfg.getInt("gatling.quickDurationSeconds") else cfg.getInt("gatling.durationSeconds")
+  val jurorFraction: Double = cfg.getDouble("gatling.jurorFraction")
+  val maxRate: Int          = cfg.getInt("gatling.maxRate")
 }
