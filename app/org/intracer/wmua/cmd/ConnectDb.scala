e78b77ac0677b0ce1469039a09f435c58a31c07e
Illia Korniiko <intracer@gmail.com>
2026-04-22 17:30:01 +0300

    code review fixes

--- app/org/intracer/wmua/cmd/ConnectDb.scala
+++ app/org/intracer/wmua/cmd/ConnectDb.scala
@@ -10,10 +10,10 @@ case class ConnectDb(host: String = "jury.wikilovesearth.org.ua",
   def apply(): Unit = {
     Class.forName("org.mariadb.jdbc.Driver")
 
-    val url = s"jdbc:mysql://$host/wlxjury"
+    val url = s"jdbc:mariadb://$host/wlxjury"
 
     val user = configuration.get[String]("db.default.user")
-    val password = configuration.get[String]("db.default.user")
+    val password = configuration.get[String]("db.default.pasword")
 
     ConnectionPool.singleton(url, user, password)
 
