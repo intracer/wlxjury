cacd044f406e91cd7aab6a56574bacf2267c1313
Illia Korniiko <intracer@gmail.com>
2026-04-25 18:38:44 +0300

    feat: register GraphQLRoute in ApiServer alongside Tapir routes

--- app/api/ApiServer.scala
+++ app/api/ApiServer.scala
@@ -1,5 +1,6 @@
 package api
 
+import graphql.GraphQLRoute
 import org.apache.pekko.actor.ActorSystem
 import org.apache.pekko.http.scaladsl.Http
 import org.apache.pekko.http.scaladsl.server.Directives.concat
@@ -11,12 +12,13 @@ import scala.concurrent.ExecutionContext
 
 @Singleton
 class ApiServer @Inject()(
-  api: Api,
-  actorSystem: ActorSystem,
-  lifecycle: ApplicationLifecycle
+  api:          Api,
+  graphQLRoute: GraphQLRoute,
+  actorSystem:  ActorSystem,
+  lifecycle:    ApplicationLifecycle
 )(implicit ec: ExecutionContext) {
 
-  private val route = concat(api.routes, api.swaggerRoute)
+  private val route = concat(api.routes, api.swaggerRoute, graphQLRoute.route)
   private val bindingFuture =
     Http(actorSystem).newServerAt("0.0.0.0", 9001).bind(Route.toFunction(route)(actorSystem))
 
